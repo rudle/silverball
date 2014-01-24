8322b0a7040ef2f195a69d167aaa16b2dccc2edc
Sean Sorrell <ssorrell@squareup.com>
2014-01-23 21:59:50 -0800

    add projectclj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject silverball "0.1.0-SNAPSHOT"
+  :description "baseball analysis workbench"
+  :url "github.com/rudle/silverball"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :jvm-opts ["-Xmx1g"]
+  )
