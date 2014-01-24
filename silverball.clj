8042fb3cf02fd9066afb24f8bcce9f71a38e18ab
Sean Sorrell <ssorrell@squareup.com>
2014-01-23 21:37:43 -0800

    add CS and SB, refactoring

--- silverball.clj
+++ silverball.clj
@@ -31,9 +31,6 @@
 (defn id [_type possible_types]
   (.indexOf  possible_types _type))
 
-(id "Strikeout" possible_event_types)
-
-
 (defn parsed-csv [csv]
   (let [raw (slurp csv)]
     (map #(clojure.string/split (clojure.string/replace % "\"" "")
@@ -43,10 +40,6 @@
 (def headers
   (clojure.string/split (slurp (str path "default_headers.txt"))
                         #"\n"))
-
-headers
-
-;(id "inning" headers)
 
 (def db
   (parsed-csv (str path "2013_EventFile.txt")))
@@ -61,8 +54,8 @@ headers
         '("Strikeout")))
 
 (defn include [l needle]
-  (boolean (some #(= needle %)
-        l)))
+  (boolean
+   (some #(= needle %) l)))
 
 (defn compact [l]
   (filter #(identity %) l))
@@ -80,26 +73,77 @@ headers
 (defn game [g db]
   (db-by "game id" g db))
 
-(defn player [p_id db]
+(defn hitter [p_id db]
   (db-by "res batter" p_id db))
 
-(defn strikeouts [db]
-  (count-by k db))
+(defn event-type [type]
+  (map str (list (id type possible_event_types))))
 
-(defn caught_stealing [db]
-  (count-by (list (id "Caught stealing" possible_event_types))
+(defn strikeouts [db]
+  (count-by (event-type "Strikeout")
             db))
 
+(defn caught_stealing [db]
+  (map #(who-was-caught-stealing %)
+       (rows-filtered-by (event-type "Caught stealing") db)))
 
 (defn count-by [event_type_keys db]
-  (count (filter #(include event_type_keys %)
-                 (map #(nth % (id "event type" headers))
-                      db))))
+  (count (filter-by event_type_keys db)))
 
-(caught_stealing (game "MIN201304010" db))
+(defn rows-filtered-by [event_type_keys db]
+  (filter #(include event_type_keys
+                    (nth % (id "event type" headers)))
+          db))
+
+(defn filter-by [event_type_keys db]
+  (filter #(include event_type_keys %)
+          (map #(nth % (id "event type" headers))
+               db)))
+
+(defn row-with-event-type [e_type]
+  (filter #(include e_type (nth % (id "event type" headers)))
+          db))
+
+(defn who-stole-a-base [sb_event]
+  (let [event_s (nth sb_event (id "event text" headers))
+        the_stolen_base
+        (second (re-find (re-pattern #"SB(\d+)")
+                         event_s))]
+    (case the_stolen_base
+      "2" (nth sb_event (id "first runner" headers))
+      "3" (nth sb_event (id "second runner" headers))
+      "H" (nth sb_event (id "third runner" headers))
+      nil)))
+
+(defn who-was-caught-stealing [cs_event]
+  (let [event_s (nth cs_event (id "event text" headers))
+        caught_stealing_at
+        (second (re-find (re-pattern #"CS(\d+)")
+                         event_s))]
+    (case caught_stealing_at
+      "2" (nth cs_event (id "first runner" headers))
+      "3" (nth cs_event (id "second runner" headers))
+      "H" (nth cs_event (id "third runner" headers))
+      nil)))
+
+(defn stolen_base [db]
+  (map #(who-stole-a-base %)
+       (rows-filtered-by (event-type "Stolen base") db)))
 
 
+;integration testing, do you do it?
 
-(= (strikeouts (player "cabrm001" (game "MIN201304010" db)))
+(= (strikeouts (hitter "cabrm001" (game "MIN201304010" db)))
    2)
 
+(=
+ (caught_stealing (game "ARI201304010" db))
+ '("parrg001"))
+
+(rows-filtered-by (event-type "Stolen base")
+                  db)
+
+(=
+ (stolen_base
+  (game "PIT201304040" db))
+ '("mccua001", "mccua001", "rizza001"))
